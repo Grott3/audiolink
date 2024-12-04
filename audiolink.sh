9cb68f14237f1c2f0f52fab55845a7f5b070e85d
Grott3 <150481018+Grott3@users.noreply.github.com>
2024-12-04 22:30:53 +0100

    fixed audiolink.sh
    closing " was missing

--- audiolink.sh
+++ audiolink.sh
@@ -3,7 +3,7 @@
 # https://obsproject.com/forum/threads/please-help-where-is-the-audio-output-going-in-obs.154475/post-565629
 
 pw-loopback \
-  --capture-props='media.class=Audio/Sink node.name=al_speaker node.description="Audiolink Speaker' \
+  --capture-props='media.class=Audio/Sink node.name=al_speaker node.description="Audiolink Speaker"' \
   --playback-props='media.class=Audio/Source node.name=al_mic node.description="Audiolink Mic"' \
   &
 
